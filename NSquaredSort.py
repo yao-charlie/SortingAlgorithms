ecf595565a2707e20cf8046d61a0142ea434a716
yao-charlie <charlieyao@gmail.com>
2020-10-30 00:50:22 -0700

    Expanded test cases to length, element intersection and brute force trial. Completed cycle sort.

--- NSquaredSort.py
+++ NSquaredSort.py
@@ -1,9 +1,9 @@
 def bubbleSort(unsorted_list):
     size = len(unsorted_list)
     for item in range(size):
-        for current in range(0, len(unsorted_list)-1-item): #less item sorted with each pass
-            if unsorted_list[current] > unsorted_list[current+1]:
-                unsorted_list[current], unsorted_list[current+1] = unsorted_list[current+1], unsorted_list[current]
+        for current in range(0, len(unsorted_list) - 1 - item):  # less item sorted with each pass
+            if unsorted_list[current] > unsorted_list[current + 1]:
+                unsorted_list[current], unsorted_list[current + 1] = unsorted_list[current + 1], unsorted_list[current]
     return unsorted_list
 
 
@@ -13,8 +13,6 @@ def insertionSort(unsorted_list):
             if unsorted_list[item_index] < unsorted_list[compare_index]:
                 unsorted_list.insert(compare_index, unsorted_list.pop(item_index))
     return unsorted_list
-
-
 
 
 def selectionSort(unsorted_list):
@@ -28,110 +26,66 @@ def selectionSort(unsorted_list):
         unsorted_list.insert(item, unsorted_list.pop(min_index))
     return unsorted_list
 
-def cycle_sort_dep(unsorted_list):
-    # 1. Select item 1, initialize value
-    item_index = 0
-    sorted_indexes = {}
-    item_value = unsorted_list[item_index]
-    loop_root = item_value #needed to not double-count in comparison
-    list_size = len(unsorted_list)
-    last_element = list_size - 1
-
-
-    # 5. How do you conclude the list is completed... visiting every element once?
-    for total_items in range(list_size+2):
-    # 4. repeat to 2, reset count
-
-        greater_than_index = 0
-
-
-        for compare_item in unsorted_list:
-            # 2. count how many items in the list should precede it (smaller than)
-            if compare_item < item_value:
-                greater_than_index += 1
-
-        if item_value > loop_root:
-            greater_than_index -= 1
-
-        # 3. put item into that location while assigning the new item as the new candidate to sort
-
-        unsorted_list.insert(greater_than_index, item_value)
-
-        last_item_index = item_index
-
-
-        item_index = greater_than_index + 1
-
-        while (item_value == unsorted_list[item_index]) and (item_index < list_size): #if the items are the same, find the next empty location to insert.
-            item_index +=1
-        item_value = unsorted_list.pop(item_index)
-
-        item_index -= 1
-
-        if last_item_index == greater_than_index:
-            item_index = (item_index+1) % list_size
-            item_value = unsorted_list[item_index]
-            loop_root = item_value
-
-        #need to prevent loops so need to keep track of sorted items - but does that increase writes, negating cycle advantage?
-        # item_index_temp = item_index
-        if item_index in sorted_indexes:
-            while item_index in sorted_indexes:
-                item_index = (item_index+1) % list_size
-
-            item_value = unsorted_list[item_index]
-            loop_root = item_value
-            #N.B could keep a variable to stop updating it every check. Doesn't improve asymptotic complexity but could improve writes
-
-
-        sorted_indexes[greater_than_index] = greater_than_index
-
-
 
 def cycleSort(unsorted_list):
+    # need check for empty list due to list index to start logic.
+    if not unsorted_list:
+        return unsorted_list
+
+    # initialize values
     item_index = 0
     sorted_indexes = {}
     item_value = unsorted_list[item_index]
     list_size = len(unsorted_list)
     loop_root = item_index
 
-
-    # 5. How do you conclude the list is completed... visiting every element once?
+    # Repeat the enclosed for n elements in the list
     for total_items in range(list_size):
-    # 4. repeat to 2, reset count
 
+        # counter tally for elements that are smaller
         greater_than_index = 0
 
-    # 2. count how many items in the list should precede it (smaller than)
+        # count how many items in the list should precede it (smaller than)
         for compare_item in range(len(unsorted_list)):
-            #skip the 1st moved element in a loop.
+            # skip the 1st moved element in a loop.
             if compare_item == loop_root:
                 continue
 
             if unsorted_list[compare_item] < item_value:
                 greater_than_index += 1
 
+        # track current index to determine if it's already in 'a' right place, if not the beginning of the right
+        # place (duplicate entries)
+        current_index = item_index
         item_index = greater_than_index
-        # 3. put item into that location while assigning the new item as the new candidate to sort
-
-        # if the items are the same, find the next empty location to insert:
-        # while (item_value == unsorted_list[item_index]) and (item_index < list_size):
-        while item_value == unsorted_list[item_index]:
+        # Find the index to insert into to sort based on the following:
+        # if the items are the same, find the next empty location to insert
+        # if you reach the same position, it's already sorted so exit loop
+        # if you reach the end of the list, it will be inserted at the end of the list.
+        while (item_value == unsorted_list[item_index]) and (item_index != current_index) and (item_index < list_size):
             item_index += 1
 
+        # Swap values
         item_value, unsorted_list[item_index] = unsorted_list[item_index], item_value
 
-    # need to prevent loops so need to keep track of sorted items - but does that increase writes, negating cycle advantage?
-        sorted_indexes[greater_than_index] = greater_than_index
+        # need to prevent loops so need to keep track of sorted items - but does that increase writes, negating cycle
+        # advantage? Necessary evil?
+
+        sorted_indexes[item_index] = item_index
+
+        # check if we've completed a loop
         if item_index == loop_root:
+            # check if we've sorted everything. Could just return instead?
+            if len(sorted_indexes) == list_size:
+                break
+            # advance through list until you find a loop you've not traversed
+            item_index += 1
             while item_index in sorted_indexes:
                 item_index = (item_index + 1) % list_size
-                if len(sorted_indexes) == list_size:
-                    break
 
+            # discard final value from previous loop by picking up the newly discovered value
             item_value = unsorted_list[item_index]
+            # re-assign to new root value.
             loop_root = item_index
-
-        # sorted_indexes[greater_than_index] = greater_than_index
 
     return unsorted_list
