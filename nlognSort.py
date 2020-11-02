2a9d103e66cafc1a8a36242137a29332b22fb5cc
yao-charlie <charlieyao@gmail.com>
2020-11-01 21:11:30 -0800

    Merge Sort implemented. Needed helper function to mutate original list.

--- nlognSort.py
+++ nlognSort.py
@@ -16,51 +16,52 @@ def quickSort(unsorted_list):
     return unsorted_list
 
 
+
 def mergeSort(unsorted_list):
+    # needed for mutating the original list in place by keeping a global variable
+
+    A = mergeSortAlgo(unsorted_list)
+
+    i = 0
+    while i < len(unsorted_list):
+        unsorted_list[i] = A[i]
+        i += 1
+
+    return unsorted_list
+
+
+
+def mergeSortAlgo(unsorted_list):
     # base case:
     if len(unsorted_list) <= 1:
         return unsorted_list
 
     # split to base cases
 
-    left_list = mergeSort(unsorted_list[0: int(len(unsorted_list) / 2)])
-    right_list = mergeSort(unsorted_list[int(len(unsorted_list) / 2): len(unsorted_list)])
+    left_list = mergeSortAlgo(unsorted_list[0: int(len(unsorted_list) / 2)])
+    right_list = mergeSortAlgo(unsorted_list[int(len(unsorted_list) / 2): len(unsorted_list)])
 
     left_index = 0
 
     # inserts right into left list
 
-    while left_index < len(left_list) or len(right_list) != 0:
-        if len(right_list) == 0:
-            break
-
-    # while left_index < len(left_list) or right_list != None:
-    #
-    #
-    #     if right_list is None:
-    #         break
-
-        elif len(unsorted_list) <= 1:
-            break
-
-        elif left_index == len(left_list):
-            left_list.append(right_list[0])
-            break
-
-        elif left_list[left_index] > right_list[0]:
-
+    while left_index < len(left_list) and len(right_list) > 0:
+        if right_list[0] <= left_list[left_index]:
             left_list.insert(left_index, right_list.pop(0))
-            left_index += 1
-
-        elif left_list[left_index] < right_list[0]:
-            left_index += 1
 
         else:
-            left_list.append(right_list.pop(0))
+            left_index += 1
 
-    if len(left_list) == len(unsorted_list):
-        unsorted_list = left_list
-        return unsorted_list
+    if len(right_list) > 0:
+        left_list.extend(right_list)
+
+
+    # if len(left_list) == len(unsorted_list):
+    #     for _ in unsorted_list:
+    #         unsorted_list[_] = left_list[_]
+    #     return unsorted_list
+
+
 
     # may not be needed - first conditional might return before this ever happens?
     return left_list
